dc6203cb4e724a3ec59086c5fe745c6d3348429d
KIMSEI1124 <workju1124@gmail.com>
2025-09-01 23:04:00 +0900

    Chore: Create Project

---
+++ src/main/java/me/imagechronicle/backend/ImageChronicleApplication.java
@@ -0,0 +1,13 @@
+package me.imagechronicle.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageChronicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageChronicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/imagechronicle/backend/ImageChronicleApplicationTests.java
@@ -0,0 +1,8 @@
+package me.imagechronicle.backend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageChronicleApplicationTests {
+
+}
